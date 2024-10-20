aba8b6254eb2ad9f5d3478999f76df3209af4bd0
khalilBout <bouternikh.khalil@gmal.com>
2024-10-20 20:42:07 +0100

    test2

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,8 +1,4 @@
 import { Outlet } from "react-router-dom";
-import ReactGA from "react-ga";
-
-const TRAFIC_ID = "G-1H2ZSZZ9EG";
-ReactGA.initialize(TRAFIC_ID);
 
 export default function Root() {
   return (
